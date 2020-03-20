695f939a50c0a00de13540df1dcd5af31e116a38
jeaninehoffman <jeanine.hoffman@bigtincan.com>
2020-03-20 13:57:33 -0700

    almost there

--- src/Components/BoardSquare.js
+++ src/Components/BoardSquare.js
@@ -2,48 +2,38 @@ import React from 'react';
 import { Square } from './';
 import { canMoveKnight, moveKnight, ItemTypes } from './Game';
 import { useDrop } from 'react-dnd';
-
+import Overlay from './Overlay'
 
 export default function BoardSquare( { x, y, children } ) {
 
     const black = (x + y) % 2 === 1;
-    const [{ isOver }, drop] = useDrop({
+    const [{ isOver, canDrop }, drop] = useDrop({
         accept: ItemTypes.KNIGHT,
+        canDrop: () => canMoveKnight(x, y),
         drop: () => moveKnight(x, y),
         collect: mon => ({
-            isOver: !!mon.isOver()
+            isOver: !!mon.isOver(),
+            canDrop: !!mon.canDrop()
         }),
 
     })
-    // const [, drop] = useDrop({
-    //     accept: ItemTypes.KNIGHT,
-    //     drop: () => moveKnight(x, y)
-    // })
+   
 
-    return  (
-            <div
-            ref={drop}
-            style={{ 
-                position: "relative",
-                width: "100%",
-                height: "100%"
-             }}>
-                <Square black={black} >
-                    {children}
-                </Square>
-                {isOver && (
-                    <div
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        height: "100%",
-                        width: "100%",
-                        zIndex: 1,
-                        opacity: 0.5,
-                        backgroundColor: "yellow"
-                    }} />
-                )}
-            </div>
-            )
+    return (
+      <div
+        ref={drop}
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <Square black={black}>{children}</Square>
+        {isOver && !canDrop && <Overlay color="red" />}
+        {!isOver && canDrop && <Overlay color="yellow" />}
+        {isOver && canDrop && <Overlay color="green" />}
+
+
+      </div>
+    );
 }
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-
-let knightPosition = [0,0];
+let knightPosition = [0, 0];
 let observer = null;
-function emitChange(){
-    observer(knightPosition);
+function emitChange() {
+  observer(knightPosition);
 }
 
 export const ItemTypes = {
-    KNIGHT: "knight",
-
-    }
+  KNIGHT: "knight"
+};
 
 // export const [{isDragging}, drag] = useDrag({
 //     item: { type: ItemTypes.KNIGHT },
@@ -22,25 +19,24 @@ export const ItemTypes = {
 
 // })
 
-
 export default function observe(o) {
-    if (observer){
-        throw new Error('multiple observers not implemented')
-    }
-    observer = o 
-    emitChange()
+  if (observer) {
+    throw new Error("multiple observers not implemented");
+  }
+  observer = o;
+  emitChange();
 }
 
-export function moveKnight(toX, toY){
-    knightPosition = [toX, toY];
-    emitChange()
+export function moveKnight(toX, toY) {
+  knightPosition = [toX, toY];
+  emitChange();
 }
-export function canMoveKnight(toX, toY){
-    const [x, y]= knightPosition;
-    const dx = toX - x;
-    const dy = toY - y;
-    return (
-        (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
-        (Math.abs(dx) === 1 && Math.abs(dy) === 2)
-    )
+export function canMoveKnight(toX, toY) {
+  const [x, y] = knightPosition;
+  const dx = toX - x;
+  const dy = toY - y;
+  return (
+    (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+    (Math.abs(dx) === 1 && Math.abs(dy) === 2)
+  );
 }
